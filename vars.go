fa2d7a9a593c86c91b2d348cfdbdeec4a1ea196d
Mark Karpeles <magicaltux@gmail.com>
2022-01-01 18:32:49 +0900

    add GetVars method for returning all vars

--- vars.go
+++ vars.go
@@ -8,3 +8,14 @@ var (
 	DATE_TAG     string = "0"
 	HOST         string = "https://dist-go.tristandev.net/"
 )
+
+func GetVars() map[string]string {
+	return map[string]string{
+		"PROJECT_NAME": PROJECT_NAME,
+		"MODE":         MODE,
+		"CHANNEL":      CHANNEL,
+		"GIT_TAG":      GIT_TAG,
+		"DATE_TAG":     DATE_TAG,
+		"HOST":         HOST,
+	}
+}
