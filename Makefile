3e74135ce9326e01b0e007cddd9336bd74832a10
Mark Karpeles <magicaltux@gmail.com>
2022-06-01 11:08:51 +0900

    use debug.ReadBuildInfo to read build information on init

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 #!/bin/make
+GOROOT:=$(shell PATH="/pkg/main/dev-lang.go.dev/bin:$$PATH" go env GOROOT)
+GOPATH:=$(shell $(GOROOT)/bin/go env GOPATH)
+
+.PHONY: test deps
 
 all:
-	goimports -w -l .
-	go build -v
+	$(GOPATH)/bin/goimports -w -l .
+	$(GOROOT)/bin/go build -v
+
+deps:
+	$(GOROOT)/bin/go get -v -t .
 
 test:
-	go test -v
+	$(GOROOT)/bin/go test -v
--- vars.go
+++ vars.go
@@ -1,5 +1,10 @@
 package goupd
 
+import (
+	"path"
+	"runtime/debug"
+)
+
 var (
 	PROJECT_NAME string = "unconfigured"
 	MODE         string = "DEV"
@@ -8,6 +13,40 @@ var (
 	DATE_TAG     string = "0"
 	HOST         string = "https://dist-go.tristandev.net/"
 )
+
+func init() {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	if PROJECT_NAME == "unconfigured" {
+		PROJECT_NAME = path.Base(bi.Path) // full path of project
+	}
+	for _, setting := range bi.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			// {Key:vcs.revision Value:dfb3603f8cb1fa40d5c3a5b9bbbdf6d316e7e2fe}
+			// check length just in case so we don't cause panic
+			if GIT_TAG == "" && len(setting.Value) >= 7 {
+				GIT_TAG = setting.Value[:7]
+			}
+		case "vcs.time":
+			if DATE_TAG != "0" {
+				break
+			}
+			// {Key:vcs.time Value:2022-06-01T01:55:46Z}
+			v := make([]byte, 0, 14) // normal length
+			for _, c := range setting.Value {
+				if c >= '0' && c <= '9' {
+					v = append(v, byte(c))
+				}
+			}
+			if len(v) == 14 {
+				DATE_TAG = string(v)
+			}
+		}
+	}
+}
 
 func GetVars() map[string]string {
 	return map[string]string{
