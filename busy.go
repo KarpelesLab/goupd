b809edb1b73e461916871bc549a6e9a61186a063
Mark Karpeles <magicaltux@gmail.com>
2022-06-12 20:26:03 +0900

    add a 1 hour timeout to busy

--- busy.go
+++ busy.go
@@ -1,6 +1,9 @@
 package goupd
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 var (
 	busyMutex sync.Mutex
@@ -9,11 +12,27 @@ var (
 )
 
 func busyLock() {
+	var s sync.Once
+	timeout := time.Now().Add(1 * time.Hour)
+
 	busyMutex.Lock()
 	for {
 		if busyState <= 0 {
 			return
 		}
+
+		if time.Until(timeout) <= 0 {
+			// let's consider this is good
+			return
+		}
+
+		s.Do(func() {
+			go func() {
+				time.Sleep(time.Until(timeout))
+				// force wake up once we reach timeout
+				busyCond.Broadcast()
+			}()
+		})
 		// other tasks are running, wait for busyState to decrease
 		busyCond.Wait()
 	}
